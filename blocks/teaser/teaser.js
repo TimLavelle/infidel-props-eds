95778fb6f4db396ae394f388f1709893c4e24958
TimLavelle <tim@lavelle.bio>
2024-08-30 20:11:17 +1000

    more efficient js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,42 +1,25 @@
-export default async function decorate(block) {
-  //   const [bg, fg] = block.children;
-  const props = [...block.children];
-  const mediaBlock = props[0];
-  const textBlock = props[1];
-  const teaserClassName = props[2].textContent.trim();
-  console.log(props);
+export default function decorate(block) {
+  const [mediaBlock, textBlock, styleBlock] = block.children;
+  const teaserClassName = styleBlock?.textContent.trim();
 
-  // Set standar Classnames
-  const teaserBody = 'teaser-body';
-  const teaserImage = 'teaser-image';
-
-  // If there's a Style assigned to the teaser, append it to the main block and remove from output
-  if (teaserClassName !== undefined) {
-    // Add the Clasname to the main block div
+  // Add style class and remove style block
+  if (teaserClassName) {
     block.classList.add(teaserClassName);
-
-    // Remove the element from Rendering
-    // TODO Would this still output to SEO and is there a better way to manage styles
-    block.lastElementChild.remove();
+    styleBlock.remove();
   }
 
-  // Decorate the Teaserbody
-  if (mediaBlock !== undefined) {
-    mediaBlock.classList.add(teaserImage);
-  }
+  // Decorate media block
+  mediaBlock?.classList.add('teaser-image');
 
-  // Decorate the Teaserbody
-  if (textBlock !== undefined) {
-    textBlock.classList.add(teaserBody);
-    const content = document.getElementsByClassName(teaserBody)[0].children[0];
-    const teaserTitle = content.querySelector('h3');
-    const teaserText = content.querySelectorAll('p');
-    const teaserPreTitle = teaserText[0];
-    const teaserDescription = teaserText[1];
+  // Decorate text block
+  if (textBlock) {
+    textBlock.classList.add('teaser-body');
 
-    teaserTitle.classList.add('teaser-title');
-    teaserPreTitle.classList.add('teaser-pre-title');
-    teaserPreTitle.setAttribute('id', 'test');
-    teaserDescription.classList.add('teaser-text');
+    const teaserTitle = textBlock.querySelector('h3');
+    const [teaserPreTitle, teaserDescription] = textBlock.querySelectorAll('p');
+
+    teaserTitle?.classList.add('teaser-title');
+    teaserPreTitle?.classList.add('teaser-pre-title');
+    teaserDescription?.classList.add('teaser-text');
   }
 }
