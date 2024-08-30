d651779854e3da57e07e516a37431b905b398600
TimLavelle <tim@lavelle.bio>
2024-08-30 20:49:26 +1000

    updated class definitions

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,30 +1,18 @@
 export default async function decorate(block) {
-  //   const [bg, fg] = block.children;
-  const [titleBlock, hasImageBlock, mediaBlock, preTitletextBlock, textBlock] = block.children;
 
+  const childElements = {
+    titleBlock: { className: 'teaser-title' },
+    hasImageBlock: { className: 'teaser-has-image' },
+    mediaBlock: { className: 'teaser-image' },
+    preTitleTextBlock: { className: 'teaser-pre-title' },
+    textBlock: { className: 'teaser-body' }
+  };
 
-  // Set standard Classnames
-  const teaserBody = 'teaser-body';
-  const teaserImage = 'teaser-image';
-
-  // Decorate the Teaserbody
-  if (mediaBlock !== undefined) {
-    mediaBlock.classList.add(teaserImage);
-  }
-
-  // Decorate the Teaserbody
-  if (textBlock) {
-    textBlock.classList.add(teaserBody);
-
-    const teaserTitle = textBlock.querySelector('h3');
-    const [teaserPreTitle, teaserDescription] = textBlock.querySelectorAll('p');
-
-    if (teaserTitle) teaserTitle.classList.add('teaser-title');
-    if (teaserPreTitle) {
-      teaserPreTitle.classList.add('teaser-pre-title');
-      // Remove this line if the 'test' id is not needed
-      // teaserPreTitle.setAttribute('id', 'test');
+  Object.entries(childElements).forEach(([key, { className }], index) => {
+    const element = block.children[index];
+    if (element) {
+      element.classList.add(className);
+      childElements[key].element = element;
     }
-    if (teaserDescription) teaserDescription.classList.add('teaser-text');
-  }
+  });
 }
