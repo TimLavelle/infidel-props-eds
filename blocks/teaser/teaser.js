28f1fedf811b7ec2e2d91628fb8b484c58e032d9
TimLavelle <tim@lavelle.bio>
2024-08-30 20:28:17 +1000

    restoring previous js

--- blocks/teaser/teaser.js
+++ blocks/teaser/teaser.js
@@ -1,25 +1,43 @@
-export default function decorate(block) {
-  const [mediaBlock, textBlock, styleBlock] = block.children;
-  const teaserClassName = styleBlock?.textContent.trim();
+export default async function decorate(block) {
+  //   const [bg, fg] = block.children;
+  const props = [...block.children];
+  const mediaBlock = props[0];
+  const textBlock = props[1];
+  const teaserClassName = props[2].textContent.trim();
+  console.log(props);
 
-  // Add style class and remove style block
-  if (teaserClassName) {
+  // Set standar Classnames
+  const teaserBody = 'teaser-body';
+  const teaserImage = 'teaser-image';
+
+  // If there's a Style assigned to the teaser, append it to the main block and remove from output
+  if (teaserClassName !== undefined) {
+    // Add the Clasname to the main block div
     block.classList.add(teaserClassName);
-    styleBlock.remove();
+
+    // Remove the element from Rendering
+    // TODO Would this still output to SEO and is there a better way to manage styles
+    block.lastElementChild.remove();
   }
 
-  // Decorate media block
-  mediaBlock?.classList.add('teaser-image');
+  // Decorate the Teaserbody
+  if (mediaBlock !== undefined) {
+    mediaBlock.classList.add(teaserImage);
+  }
 
-  // Decorate text block
+  // Decorate the Teaserbody
   if (textBlock) {
-    textBlock.classList.add('teaser-body');
+    textBlock.classList.add(teaserBody);
 
     const teaserTitle = textBlock.querySelector('h3');
     const [teaserPreTitle, teaserDescription] = textBlock.querySelectorAll('p');
 
-    teaserTitle?.classList.add('teaser-title');
-    teaserPreTitle?.classList.add('teaser-pre-title');
-    teaserDescription?.classList.add('teaser-text');
+    if (teaserTitle) teaserTitle.classList.add('teaser-title');
+    if (teaserPreTitle) {
+      teaserPreTitle.classList.add('teaser-pre-title');
+      // Remove this line if the 'test' id is not needed
+      // teaserPreTitle.setAttribute('id', 'test');
+    }
+    if (teaserDescription) teaserDescription.classList.add('teaser-text');
   }
 }
