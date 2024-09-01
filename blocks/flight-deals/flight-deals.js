a29b3ab6c3e49b672b74e0338ea3d719154b7ce2
TimLavelle <tim@lavelle.bio>
2024-09-01 11:08:26 +1000

    parameterising the api string

--- blocks/flight-deals/flight-deals.js
+++ blocks/flight-deals/flight-deals.js
@@ -1,4 +1,6 @@
 import { addBlockClasses, removeUtilityElements } from '../../utils/blockUtils.js';
+import { createOptimizedPicture, decorateButtons } from '../../scripts/aem.js';
+import { moveInstrumentation } from '../../scripts/scripts.js';
 
 export default async function decorate(block) {
 
@@ -6,30 +8,30 @@ export default async function decorate(block) {
     { key: 'title', className: 'deals-title' },
     { key: 'fromPort', className: 'deals-from-port' },
     { key: 'travelClass', className: 'deals-travel-class' },
-    { key: 'showDealImages', className: 'deals-show-deal-images' }
+    { key: 'showDealImages', className: 'deals-show-deal-images' },
+    { key: 'showDisclaimers', className: 'deals-show-disclaimers' }
   ];
 
   addBlockClasses(block, childElements);
-  removeUtilityElements(childElements, ['fromPort', 'travelClass', 'showDealImages']);
+  removeUtilityElements(childElements, ['fromPort', 'travelClass', 'showDealImages', 'showDisclaimers']);
 
-  const dealsAPI = 'https://www.qantas.com/api/flightOffers/v2/offers?departureAirport=SYD&includeDisclaimers=false&saleName=London%20and%20Paris%20Red%20Tail%20Sale&destination=CDG:ECONOMY&destination=LHR:ECONOMY';  const ul = document.createElement('ul');
+  const fromPort = block.querySelector('.deals-from-port').textContent;
+  const showDisclaimers = block.querySelector('.deals-show-disclaimers').textContent;
+  
+  const dealsAPI = 'https://www.qantas.com/api/flightOffers/v2/offers?departureAirport=' + fromPort + '&includeDisclaimers=' + showDisclaimers + '&saleName=London%20and%20Paris%20Red%20Tail%20Sale&destination=CDG:ECONOMY&destination=LHR:ECONOMY';  
   const deals = await fetch(dealsAPI).then(res => res.json());
 
+  const ul = document.createElement('ul');
   let dealsItems = '';
 
   deals.offers.forEach(offer => {
     dealsItems += `
-      <li>
-        <div class="deal-item">
-          <div class="deal-item-image">
-            image placeholder
-          </div>
-          <div class="deal-item-content">
-            ${offer.sale && typeof offer.sale === 'object' && offer.sale.name ? `<p>${offer.sale.name}</p>` : ''}
-            <p><strong>${offer.route.from.name}</strong></p>
-            <p class="deal-item-type">${offer.travelClass + offer.tripType} from</p>
-            <p>${offer.price.symbol} ${offer.price.amountFormatted}</p>
-          </div>
+      <li class="deal-item">
+        <div class="deal-item-content">
+          ${offer.sale && typeof offer.sale === 'object' && offer.sale.name ? `<p>${offer.sale.name}</p>` : ''}
+          <p><strong>${offer.route.from.name}</strong></p>
+          <p class="deal-item-type">${offer.travelClass + ' ' + offer.tripType} from</p>
+          <p>${offer.price.symbol} ${offer.price.amountFormatted}</p>
         </div>
       </li>
     `;
