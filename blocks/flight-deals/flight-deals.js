466ba3ff47578472ca00f2a405a0551c848c0f24
TimLavelle <tim@lavelle.bio>
2024-09-01 10:23:09 +1000

    updated js

--- blocks/flight-deals/flight-deals.js
+++ blocks/flight-deals/flight-deals.js
@@ -1,37 +1,17 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
-export default function decorate(block) {
-  const ul = document.createElement('ul');
+export default async function decorate(block) {
 
-  fetch('https://www.qantas.com/api/flightOffers/v2/offers?departureAirport=SYD&includeDisclaimers=false&saleName=London%20and%20Paris%20Red%20Tail%20Sale&destination=CDG:ECONOMY&destination=LHR:ECONOMY')
-    .then(response => response.json())
-    .then(data => {
-      data.offers.forEach(offer => {
-        const li = document.createElement('li');
-        moveInstrumentation(block, li);
-        
-        const p1 = document.createElement('p');
-        p1.textContent = `Destination: ${offer.route.to.name}`;
-        li.appendChild(p1);
+  const dealsAPI = 'https://www.qantas.com/api/flightOffers/v2/offers?departureAirport=SYD&includeDisclaimers=false&saleName=London%20and%20Paris%20Red%20Tail%20Sale&destination=CDG:ECONOMY&destination=LHR:ECONOMY';  const ul = document.createElement('ul');
+  const deals = await fetch(dealsAPI).then(res => res.json());
 
-        const p2 = document.createElement('p');
-        p2.textContent = `Travel Class: ${offer.travelClass}`;
-        li.appendChild(p2);
+  let dealsItems = '';
 
-        const p3 = document.createElement('p');
-        p3.textContent = `Trip Type: ${offer.tripType}`;
-        li.appendChild(p3);
+  deals.offers.forEach(offer => {
+    dealsItems += `<li>${offer.route.to.name}</li>`;
+  });
 
-        const p4 = document.createElement('p');
-        p4.textContent = `Fare Family: ${offer.fareFamily}`;
-        li.appendChild(p4);
-        
-        ul.appendChild(li);
-        console.log(offer);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching flight deals:', error);
-    });
+  ul.innerHTML = dealsItems;
+  block.append(ul);
 }
